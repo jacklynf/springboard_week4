2514dcb150eab4c2d357b60dc6041d017a47def4
jackie_aws <jackie@aws_workspace>
2023-02-18 21:36:29 -0800

    vector complete

--- create_vector/create_vector.cc
+++ create_vector/create_vector.cc
@@ -5,10 +5,10 @@ using namespace std;
 
 int main ()
 {
-  // vector<int> v{10,5};
+  //vector<int> v{10,5};
   // vector<int> v = {10,5};
   // vector<int> v(10,5);
-  // vector<int> v = (10,5);
+   vector<int> v = (10,5);
 
   for (auto& elem : v) {
     cout << elem << " ";
